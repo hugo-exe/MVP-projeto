e11e027995747606b8b3c4e105c78157f30defc3
Hugo <113729299+hugo-exe@users.noreply.github.com>
2023-06-12 16:17:21 -0300

    commit 2

---
+++ SFD – Sistema de Finanças Domésticas/src/br/com/sfd/mvp/models/prospeccao.java
@@ -0,0 +1,37 @@
+public class prospeccao {
+    private int idProspeccao;
+    private String descricao;
+    private float valor;
+    private int parcelas;
+
+    public prospeccao (){
+    }
+
+    public prospeccao (int idProspeccao, String descricao, float valor, int parcelas) {
+        super();
+        this.idProspeccao = idProspeccao;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.parcelas = parcelas;
+    }
+
+    public void descricaoSimulador() {
+        System.out.println("Descrição do Simulador: " + descricao);
+    }
+    
+    public void inserirValor(float valor) {
+        this.valor = valor;
+    }
+    
+    public void inserirParcelas(int parcelas) {
+        this.parcelas = parcelas;
+    }
+    
+    public void executarProspeccao() {
+        System.out.println("Executando prospecção...");
+        System.out.println("ID: " + idProspeccao);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("Parcelas: " + parcelas);
+    }
+}
